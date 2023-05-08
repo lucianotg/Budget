4f7a31928d8c7759540659a9ec3408a462b5120e
lucianotg <luciano@takegive.net>
2023-05-07 20:57:00 -0500

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Luciano Patino on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
